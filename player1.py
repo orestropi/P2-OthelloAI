fc49f41b728a2943be27d4630f7015cf16c09b39
orestropi <oropi@wpi.edu>
2021-09-29 20:41:39 -0400

    made basic evalF

--- player1.py
+++ player1.py
@@ -6,36 +6,30 @@ import time
 
 
 def get_valid_moves(board: Board, color: PieceColor) -> list:
-    """
-    Check if there exists a valid move for the given color
-    :param color: PieceColor
-    :return: list of valid moves, empty list if not
-    """
-    moves = []
-    for row in range(8):
-        for col in range(8):
-            piece = Board._get_piece(board, row, col)
-            # print(board.board[row * 8 + col])
-            # print(row, col)
-            # print(len(board.board))
-            #print(board.board[row * 8 + col])
-            #print(row, col)
-            #print(len(board.board))
-            if (piece == PieceColor.NONE) and len(Board._get_enveloped_pieces(board, row, col, color)) > 0:
+   """
+   Check if there exists a valid move for the given color
+   :param color: PieceColor
+   :return: list of valid moves, empty list if not
+   """
+   moves = []
+   for row in range(8):
+       for col in range(8):
+           piece = Board._get_piece(board, row, col)
+           # print(board.board[row * 8 + col])
+           # print(row, col)
+           # print(len(board.board))
+           #print(board.board[row * 8 + col])
+           #print(row, col)
+           #print(len(board.board))
+           if (piece == PieceColor.NONE) and len(Board._get_enveloped_pieces(board, row, col, color)) > 0:
 
-                moves.append([row,col])
-    print(moves)
-    return moves
-
-
-'''input: the board, the player color that is going next
-result: positive float means better for orange, negative float means better for blue'''
-
+               moves.append([row,col])
+   print(moves)
+   return moves
 
 def eval_func(board: Board, colorE: PieceColor) -> float:
     evalF = 0
     numF = 0
-
     """
     Get opponent of given player
     :param player: Board currently being played
@@ -60,10 +54,18 @@ def eval_func(board: Board, colorE: PieceColor) -> float:
                                                ((rowE == 1) and (col == 8)) or
                                                ((rowE == 8) and (col == 8))):
                 evalF -= 5
-            if piece == PieceColor.NONE:
+            if (piece == PieceColor.NONE):
                 numF += 1
 
     numOfPieces = 64 - numF
+
+    '''factor 3: number of available moves are important to winning an othello game'''
+    if colorE == PieceColor.ORANGE:
+        evalF += get_valid_moves(board, colorE).__sizeof__() / numOfPieces
+    if colorE == PieceColor.BLUE:
+        evalF -= get_valid_moves(board, colorE).__sizeof__() / numOfPieces
+
+    return evalF
 
 
 #board = [PieceColor.NONE] * 64
@@ -88,78 +90,77 @@ print("Initial Board:\n{b}\n".format(b=game.board))
 first = True #flag used to get the color of the player
 #while the game is being played
 while True:
-    while os.path.isfile("./referee/Player1.go"): #get the file the referee made
-        if first:
-            #if nothing in the move file, the player's color is blue
-            if os.stat("./referee/move_file").st_size==0:
-                print("I am first player")
-                color = PieceColor.BLUE
-                OColor = PieceColor.ORANGE
-                first = False #set flag to false so do not check the color again
-            #if there is already a line in the move file and the flag is still true, the player's color is orange
-            else:
-                print("I am second player")
-                color = PieceColor.ORANGE
-                OColor = PieceColor.BLUE
-                first = False
+   while os.path.isfile("./referee/Player1.go"): #get the file the referee made
+       if first:
+           #if nothing in the move file, the player's color is blue
+           if os.stat("./referee/move_file").st_size==0:
+               print("I am first player")
+               color = PieceColor.BLUE
+               OColor = PieceColor.ORANGE
+               first = False #set flag to false so do not check the color again
+           #if there is already a line in the move file and the flag is still true, the player's color is orange
+           else:
+               print("I am second player")
+               color = PieceColor.ORANGE
+               OColor = PieceColor.BLUE
+               first = False
 
 
-        #we first need to update the board by reading move file
-        #mtime = os.path.getmtime("./referee/move_file")
-        #modified = False
+       #we first need to update the board by reading move file
+       #mtime = os.path.getmtime("./referee/move_file")
+       #modified = False
 
-        #if os.path.getmtime("./referee/move_file") > mtime:
-            #modified = True
-            #break
+       #if os.path.getmtime("./referee/move_file") > mtime:
+           #modified = True
+           #break
 
-        #if modified:
-        with open("./referee/move_file", "r") as fp:
-                # Get last non-empty line from file
-            line = ""
-            for next_line in fp.readlines():
-                if next_line.isspace():
-                    break
-                else:
-                    line = next_line
+       #if modified:
+       with open("./referee/move_file", "r") as fp:
+               # Get last non-empty line from file
+           line = ""
+           for next_line in fp.readlines():
+               if next_line.isspace():
+                   break
+               else:
+                   line = next_line
 
-                # Tokenize move
-            if not line=="":
-                tokens = line.split()
-                group_name = tokens[0]
-                col = tokens[1]
-                row = tokens[2]
+               # Tokenize move
+           if not line=="":
+               tokens = line.split()
+               group_name = tokens[0]
+               col = tokens[1]
+               row = tokens[2]
 
-                game.board.set_piece(int(row), str(col), OColor) #updates board
+               game.board.set_piece(int(row), str(col), OColor) #updates board
 
 
-        fp.close()
+       fp.close()
 
-        #first check if board is full -- do we need to do this??
-        #then check if there are any possible moves
+       #first check if board is full -- do we need to do this??
+       #then check if there are any possible moves
 
-        #get possible moves and put those coordinates in list
-        moves = get_valid_moves(game.board, color)
-        # get the number of pieces it would change for each possible move and store in array
-        rankedMoves = []
-        for index in range(len(moves)):
-            changedMoves = Board._get_enveloped_pieces(game.board, moves[index][0], moves[index][1], color)
-            numChanged = len(changedMoves)
+       #get possible moves and put those coordinates in list
+       moves = get_valid_moves(game.board, color)
+       # get the number of pieces it would change for each possible move and store in array
+       rankedMoves = []
+       for index in range(len(moves)):
+           changedMoves = Board._get_enveloped_pieces(game.board, moves[index][0], moves[index][1], color)
+           numChanged = len(changedMoves)
 
-            rankedMoves.append([moves[index][0], moves[index][1], numChanged])
-        rankedMoves.sort(key=lambda tup: tup[2], reverse=True)
-        print("Ranked Moves:", rankedMoves)
-        # sort this list and use the move with the most possible moves
+           rankedMoves.append([moves[index][0], moves[index][1], numChanged])
+       rankedMoves.sort(key=lambda tup: tup[2], reverse=True)
+       print("Ranked Moves:", rankedMoves)
+       # sort this list and use the move with the most possible moves
 
-        topMove = rankedMoves[0]
-        row = topMove[0]
-        (rowC, colC) = transform_coords(row, topMove[1])
-        f = open("./referee/move_file", 'a')  # open the move file to make a move
-        # write the desired move in the move file
-        #print("GroupX ", str(topMove[1]), " ", str(row))
-        #f.write("GroupX E 3")
-        stringToWrite = ('Player1 ' + str(colC) + " " + str(rowC)+ "\n")
-        f.write(stringToWrite)  # write the desired move in the move file
-        f.close()  # close the file until need to wirte to it again
-        game.board.set_piece(rowC, colC, color) #update our board
-        time.sleep(1);
-
+       topMove = rankedMoves[0]
+       row = topMove[0]
+       (rowC, colC) = transform_coords(row, topMove[1])
+       f = open("./referee/move_file", 'a')  # open the move file to make a move
+       # write the desired move in the move file
+       #print("GroupX ", str(topMove[1]), " ", str(row))
+       #f.write("GroupX E 3")
+       stringToWrite = ('Player1 ' + str(colC) + " " + str(rowC)+ "\n")
+       f.write(stringToWrite)  # write the desired move in the move file
+       f.close()  # close the file until need to wirte to it again
+       game.board.set_piece(rowC, colC, color) #update our board
+       time.sleep(1);
--- player2.py
+++ player2.py
@@ -1,3 +1,4 @@
+#
 import os.path
 import sys
 from Board import PieceColor, Board, transform_coords
@@ -5,27 +6,66 @@ from referee.Game import Game
 import time
 
 
-def get_valid_moves(board: Board, color: PieceColor) -> bool:
-    """
-    Check if there exists a valid move for the given color
-    :param color: PieceColor
-    :return: list of valid moves, empty list if not
-    """
-    moves = []
-    for row in range(8):
-        for col in range(8):
-            piece = Board._get_piece(board, row, col)
-            # print(board.board[row * 8 + col])
-            # print(row, col)
-            # print(len(board.board))
-            # print(board.board[row * 8 + col])
-            # print(row, col)
-            # print(len(board.board))
-            if (piece == PieceColor.NONE) and len(Board._get_enveloped_pieces(board, row, col, color)) > 0:
-                moves.append([row, col])
-    print(moves)
-    return moves
+def get_valid_moves(board: Board, color: PieceColor) -> list:
+   """
+   Check if there exists a valid move for the given color
+   :param color: PieceColor
+   :return: list of valid moves, empty list if not
+   """
+   moves = []
+   for row in range(8):
+       for col in range(8):
+           piece = Board._get_piece(board, row, col)
+           # print(board.board[row * 8 + col])
+           # print(row, col)
+           # print(len(board.board))
+           # print(board.board[row * 8 + col])
+           # print(row, col)
+           # print(len(board.board))
+           if (piece == PieceColor.NONE) and len(Board._get_enveloped_pieces(board, row, col, color)) > 0:
+               moves.append([row, col])
+   print(moves)
+   return moves
 
+def eval_func(board: Board, colorE: PieceColor) -> float:
+    evalF = 0
+    numF = 0
+    """
+    Get opponent of given player
+    :param player: Board currently being played
+    :return: evaluation for current position
+    """
+    for rowE in range(8):
+        for col in range(8):
+            piece = Board._get_piece(board, rowE, col)
+            '''factor 1: number of pieces for each color are a consideration to winning an othello game'''
+            if piece == PieceColor.ORANGE:
+                evalF += .02
+            if piece == PieceColor.BLUE:
+                evalF -= .02
+            '''factor 2: corner pieces are crucial to winning an  othello game'''
+            if (piece == PieceColor.ORANGE) and (((rowE == 1) and (col == 1)) or
+                                                 ((rowE == 8) and (col == 1)) or
+                                                 ((rowE == 1) and (col == 8)) or
+                                                 ((rowE == 8) and (col == 8))):
+                evalF += 5
+            if (piece == PieceColor.BLUE) and (((rowE == 1) and (col == 1)) or
+                                               ((rowE == 8) and (col == 1)) or
+                                               ((rowE == 1) and (col == 8)) or
+                                               ((rowE == 8) and (col == 8))):
+                evalF -= 5
+            if (piece == PieceColor.NONE):
+                numF += 1
+
+    numOfPieces = 64 - numF
+
+    '''factor 3: number of available moves are important to winning an othello game'''
+    if colorE == PieceColor.ORANGE:
+        evalF += get_valid_moves(board, colorE).__sizeof__() / numOfPieces
+    if colorE == PieceColor.BLUE:
+        evalF -= get_valid_moves(board, colorE).__sizeof__() / numOfPieces
+
+    return evalF
 
 # board = [PieceColor.NONE] * 64
 
@@ -49,75 +89,75 @@ print("Initial Board:\n{b}\n".format(b=game.board))
 first = True  # flag used to get the color of the player
 # while the game is being played
 while True:
-    while os.path.isfile("./referee/Player2.go"):  # get the file the referee made
-        if first:
-            # if nothing in the move file, the player's color is blue
-            if os.stat("./referee/move_file").st_size == 0:
-                print("I am first player")
-                color = PieceColor.BLUE
-                OColor = PieceColor.ORANGE
-                first = False  # set flag to false so do not check the color again
-            # if there is already a line in the move file and the flag is still true, the player's color is orange
-            else:
-                print("I am second player")
-                color = PieceColor.ORANGE
-                OColor = PieceColor.BLUE
-                first = False
+   while os.path.isfile("./referee/Player2.go"):  # get the file the referee made
+       if first:
+           # if nothing in the move file, the player's color is blue
+           if os.stat("./referee/move_file").st_size == 0:
+               print("I am first player")
+               color = PieceColor.BLUE
+               OColor = PieceColor.ORANGE
+               first = False  # set flag to false so do not check the color again
+           # if there is already a line in the move file and the flag is still true, the player's color is orange
+           else:
+               print("I am second player")
+               color = PieceColor.ORANGE
+               OColor = PieceColor.BLUE
+               first = False
 
-        # we first need to update the board by reading move file
-        # mtime = os.path.getmtime("./referee/move_file")
-        # modified = False
+       # we first need to update the board by reading move file
+       # mtime = os.path.getmtime("./referee/move_file")
+       # modified = False
 
-        # if os.path.getmtime("./referee/move_file") > mtime:
-        # modified = True
-        # break
+       # if os.path.getmtime("./referee/move_file") > mtime:
+       # modified = True
+       # break
 
-        # if modified:
-        with open("./referee/move_file", "r") as fp:
-            # Get last non-empty line from file
-            line = ""
-            for next_line in fp.readlines():
-                if next_line.isspace():
-                    break
-                else:
-                    line = next_line
+       # if modified:
+       with open("./referee/move_file", "r") as fp:
+           # Get last non-empty line from file
+           line = ""
+           for next_line in fp.readlines():
+               if next_line.isspace():
+                   break
+               else:
+                   line = next_line
 
-                # Tokenize move
-            if not line == "":
-                tokens = line.split()
-                group_name = tokens[0]
-                col = tokens[1]
-                row = tokens[2]
+               # Tokenize move
+           if not line == "":
+               tokens = line.split()
+               group_name = tokens[0]
+               col = tokens[1]
+               row = tokens[2]
 
-                game.board.set_piece(int(row), str(col), OColor)  # updates board
+               game.board.set_piece(int(row), str(col), OColor)  # updates board
 
-        fp.close()
+       fp.close()
 
-        # first check if board is full -- do we need to do this??
-        # then check if there are any possible moves
+       # first check if board is full -- do we need to do this??
+       # then check if there are any possible moves
 
-        # get possible moves and put those coordinates in list
-        moves = get_valid_moves(game.board, color)
-        # get the number of pieces it would change for each possible move and store in array
-        rankedMoves = []
-        for index in range(len(moves)):
-            changedMoves = Board._get_enveloped_pieces(game.board, moves[index][0], moves[index][1], color)
-            numChanged = len(changedMoves)
+       # get possible moves and put those coordinates in list
+       moves = get_valid_moves(game.board, color)
+       # get the number of pieces it would change for each possible move and store in array
+       rankedMoves = []
+       for index in range(len(moves)):
+           changedMoves = Board._get_enveloped_pieces(game.board, moves[index][0], moves[index][1], color)
+           numChanged = len(changedMoves)
 
-            rankedMoves.append([moves[index][0], moves[index][1], numChanged])
-        rankedMoves.sort(key=lambda tup: tup[2], reverse=True)
-        print("Ranked Moves:", rankedMoves)
-        # sort this list and use the move with the most possible moves
+           rankedMoves.append([moves[index][0], moves[index][1], numChanged])
+       rankedMoves.sort(key=lambda tup: tup[2], reverse=True)
+       print("Ranked Moves:", rankedMoves)
+       # sort this list and use the move with the most possible moves
 
-        topMove = rankedMoves[0]
-        row = topMove[0]
-        (rowC, colC) = transform_coords(row, topMove[1])
-        f = open("./referee/move_file", 'a')  # open the move file to make a move
-        # write the desired move in the move file
-        # print("GroupX ", str(topMove[1]), " ", str(row))
-        # f.write("GroupX E 3")
-        stringToWrite = ('Player2 ' + str(colC) + " " + str(rowC)+ "\n")
-        f.write(stringToWrite)  # write the desired move in the move file
-        f.close()  # close the file until need to wirte to it again
-        game.board.set_piece(rowC, colC, color)  # update our board
-        time.sleep(1);
+       topMove = rankedMoves[0]
+       row = topMove[0]
+       (rowC, colC) = transform_coords(row, topMove[1])
+       f = open("./referee/move_file", 'a')  # open the move file to make a move
+       # write the desired move in the move file
+       # print("GroupX ", str(topMove[1]), " ", str(row))
+       # f.write("GroupX E 3")
+       stringToWrite = ('Player2 ' + str(colC) + " " + str(rowC)+ "\n")
+       f.write(stringToWrite)  # write the desired move in the move file
+       f.close()  # close the file until need to wirte to it again
+       game.board.set_piece(rowC, colC, color)  # update our board
+       time.sleep(1);
